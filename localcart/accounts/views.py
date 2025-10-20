02a83043ccbc69ccdfd5da2f3c9d52e7ffd7c065
rewanalahmad <rewan.a.alahmad@gmail.com>
2025-10-20 12:42:58 +0300

    4

--- localcart/accounts/views.py
+++ localcart/accounts/views.py
@@ -3,10 +3,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.http import HttpResponse
 from django.template import loader
+from .forms import UserProfileForm
 
-# Create your views here.
 def register(request):
-    template=loader.get_template("register.html")
     if  request.method !='POST':
         form=UserCreationForm()
     else:
@@ -17,4 +16,15 @@ def register(request):
             login(request,new_user)
             return HttpResponse("Done")
     context={'form':form}
-    return render(request, 'register.html', {'form': form})
+    return render(request,'register.html',context)
+
+def edit_user_profile(requeast):
+    if requeast.method=='POST':
+        form=UserProfileForm(requeast.POST, requeast.FILES ,
+                             instance=requeast.user.profile)#files bec we allowed users add files.
+        if form.is_valid():
+            form.save()
+            return HttpResponse('profile was updated !!')
+    else:
+        form=UserProfileForm(instance=requeast.user.profile)
+    return render(requeast,'edit_profile.html',{'form':form})
