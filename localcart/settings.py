d8708bed6cefbd711a8df55867d5ffd528180180
rewanalahmad <rewan.a.alahmad@gmail.com>
2025-10-31 17:09:12 +0300

    1

--- localcart/settings.py
+++ localcart/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lr7t4_*baq77gq%hz%12im#nxw)zc7)9h81jw%-d3*+ht0p-i$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
