89d107cf0e4711b6e411f55a1fd310099b25273a
rewanalahmad <rewan.a.alahmad@gmail.com>
2025-11-29 13:18:34 +0300

    pytest

--- tests/test_screenshots.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-import pytest
-from selenium import webdriver
-
-def take_screenshot(driver,name):
-    time.sleep(1)
-    os.makedirs(os.path.join('screenshot',os.path.dirname(name)),exist_ok=True)
-    driver.save_screenshot(os.path.join('screenshot',name))
